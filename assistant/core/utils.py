b6087f506d879d67414a6ccab4597fc4d313acc8
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-09-02 09:24:38 +0100

    backend code commit by ENGR. IPAYE

--- assistant/core/utils.py
+++ assistant/core/utils.py
@@ -1,2 +1,11 @@
 import pyttsx3
 
+_tts_engine = None
+
+def speak(text: str):
+    global _tts_engine
+    if _tts_engine is None:
+        _tts_engine = ppttsx3.init()
+        _tts_engine.setProperty('rate', 185)
+    _tts_engine.say(text)
+    _tts_engine.runAndWait()
